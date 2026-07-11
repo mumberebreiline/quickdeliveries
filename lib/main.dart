c64f0749c7af4af02358a66c09cfee5d9f71cf3e
Eric-zard <wanderaerickson01@gmail.com>
2026-07-11 15:12:59 -0800

    error fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'screens/home/home_screen.dart'; // Adjust path to match your file structure
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
