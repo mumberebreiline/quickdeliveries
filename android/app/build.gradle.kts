f9c2d515090342ba55acf5f0a196979de3398c95
mumberebreiline <breilinemumbere@gmail.com>
2026-07-11 08:35:31 +0300

    configure firebase and cloud fire

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,4 +43,23 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+dependencies {
+
+  // Import the Firebase BoM
+
+  implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+
+  // When using the BoM, don't specify versions in Firebase dependencies
+
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+
+  // https://firebase.google.com/docs/android/setup#available-libraries
+
 }
