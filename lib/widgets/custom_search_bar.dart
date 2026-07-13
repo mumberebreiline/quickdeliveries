86b134f192e3e7634480766cd921fc3fc6bb16c2
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-07-13 12:39:43 +0300

    accepting new changes

---
+++ lib/widgets/custom_search_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search for food...",
+        prefixIcon: const Icon(Icons.search),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
