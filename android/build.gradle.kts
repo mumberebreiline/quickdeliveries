585f1f2d1f4484eb1c3bec2ea7b24426e2a85541
Eric-zard <wanderaerickson01@gmail.com>
2026-07-16 15:26:12 -0800

    added more options to menu screen

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,9 +4,11 @@ allprojects {
         mavenCentral()
     }
 }
- dependencies{
-   classpath 'com.google.gms.google-services' version '4.5.0' apply false
- }
+plugins {
+    // Other existing plugins...
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
