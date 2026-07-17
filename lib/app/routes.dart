4d7cdcaf8377d29830c2b394532b9f5b7043234e
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-07-17 23:40:55 +0300

    new pull files

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -8,6 +8,8 @@ import '../screens/vendor/dashboard_screen.dart';
 import '../screens/vendor/incoming_orders_screen.dart';
 import '../screens/vendor/route_map_screen.dart';
 import '../screens/vendor/delivery_history_screen.dart';
+import '../screens/vendor/hazards_screen.dart';
+import '../screens/customer/customer_login_screen.dart';
 
 /// Every route name lives here. Add one line here + one in [appRoutes]
 /// whenever a new screen is ready to be linked in.
@@ -28,6 +30,8 @@ class AppRoutes {
   static const String vendorIncoming = '/vendor-incoming';
   static const String vendorRouteMap = '/vendor-route-map';
   static const String vendorHistory = '/vendor-history';
+  static const String vendorHazards = '/vendor-hazards';
+  static const String customerLogin = '/customer-login';
 }
 
 /// Maps each route name to the screen that should be shown.
@@ -35,4 +39,12 @@ class AppRoutes {
 final Map<String, WidgetBuilder> appRoutes = {
   AppRoutes.splash: (context) => const SplashScreen(),
   AppRoutes.home: (context) => const HomeScreen(),
+  AppRoutes.order: (context) => const OrderScreen(),
+  AppRoutes.vendorLogin: (context) => const VendorLoginScreen(),
+  AppRoutes.vendorDashboard: (context) => const VendorDashboardScreen(),
+  AppRoutes.vendorIncoming: (context) => const IncomingOrdersScreen(),
+  AppRoutes.vendorRouteMap: (context) => const RouteMapScreen(),
+  AppRoutes.vendorHistory: (context) => const DeliveryHistoryScreen(),
+  AppRoutes.vendorHazards: (context) => const HazardsScreen(),
+  AppRoutes.customerLogin: (context) => const CustomerLoginScreen(),
 };
